dd31798465a59a390b5828c078c60bc1c7913b15
Seoyeong-Kim <seoyeong2352@gmail.com>
2019-06-06 12:18:43 +0900

    [HackerRank] Algorithm - Mini-Max Sum

---
+++ src/test/java/hackerrank/minimaxsum/SolutionTest.java
@@ -0,0 +1,10 @@
+package hackerrank.minimaxsum;
+
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void miniMaxSum() {
+        Solution.miniMaxSum(new int[]{1, 2, 3, 4, 5});
+    }
+}
