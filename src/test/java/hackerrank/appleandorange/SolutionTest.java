4ecc52e5e78c868faff2625d682c9701621b2477
Seoyeong-Kim <seoyeong2352@gmail.com>
2019-06-19 09:05:05 +0900

    [HackerRank] Algorithm - Apples and Orange

---
+++ src/test/java/hackerrank/appleandorange/SolutionTest.java
@@ -0,0 +1,11 @@
+package hackerrank.appleandorange;
+
+import org.junit.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void AppleAndOrange() {
+        Solution.countApplesAndOranges(7, 11, 5, 15, new int[]{-2, 2, 1}, new int[]{5, -6});
+    }
+}
